66edcedda4bbe7a99ed9d8d89aa217642629c2be
Bruce <joqtdepy@duck.com>
2024-07-23 12:49:48 +0800

    feat: NavLinks

--- apps/web/app/dashboard/layout.tsx
+++ apps/web/app/dashboard/layout.tsx
@@ -1,12 +1,12 @@
 
-// import SideNav from '@/app/ui/dashboard/sidenav';
+import NavLinks from './nav-links';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
-        {/* <SideNav /> */}
         <p>Dashboard Layout</p>
+        <NavLinks />
       </div>
       <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
     </div>
