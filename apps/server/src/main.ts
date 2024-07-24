c96e11555b9709b733fe065d6cbf87af1ee8991f
Bruce <joqtdepy@duck.com>
2024-07-24 14:24:12 +0800

    fix: deployment error.

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -3,16 +3,21 @@ import { AppModule } from './app.module';
 import { TrpcRouter } from '@server/trpc/trpc.router';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  try {
+    const app = await NestFactory.create(AppModule);
+    app.enableCors();
 
-  const trpc = app.get(TrpcRouter);
-  trpc.applyMiddleware(app);
+    const trpc = app.get(TrpcRouter);
+    trpc.applyMiddleware(app);
 
-  // const port = process.env.PORT;
-  // console.log(`[server]: Server is running at http://localhost:${port}`);
+    // const port = process.env.PORT;
+    // console.log(`[server]: Server is running at http://localhost:${port}`);
 
-  // await app.listen(process.env.PORT || 4000);
-  await app.listen(4000);
+    // await app.listen(process.env.PORT || 4000);
+    await app.listen(4000);
+  } catch (error) {
+    console.error('Failed to start the application:', error);
+    process.exit(1); // quit.
+  }
 }
 bootstrap();
